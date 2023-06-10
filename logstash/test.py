42b905fc283d9e88e8a11c1fc2ddbd6ad4e5bf86
Nik Jakimovec <imkolias@gmail.com>
2023-06-10 21:00:33 +0300

    Sprint9 logs almost done

--- logstash/test.py
+++ logstash/test.py
@@ -2,5 +2,5 @@
 
 import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.sendto(b'99 54 55', 0, ('127.0.0.1', 5044))
+sock.sendto(b'127001 - - [09/Jun/2023:11:54:31 +0000] "GET /api/v1/status HTTP/11" 503 43 "-" "curl/7740"', 0, ('127.0.0.1', 12201))
 sock.close()
