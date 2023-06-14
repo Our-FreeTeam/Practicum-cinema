6a3f8d8b8fd1b855a97394533793fad192223fa5
Nik Jakimovec <imkolias@gmail.com>
2023-06-14 21:35:05 +0300

    Sprint9 likes and tests
    Took 5 minutes

--- analytic-solution/api/v1/views.py
+++ analytic-solution/api/v1/views.py
@@ -1,10 +1,9 @@
 from uuid import UUID
 
 from auth_service import is_authorized
+from db.redis import get_redis
 from fastapi import APIRouter, Depends, Request
 from models.models import UserLike, UserView
-
-from db.redis import get_redis
 
 router = APIRouter()
 
