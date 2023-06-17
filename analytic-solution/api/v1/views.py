36e1e1ed620847e33efe72f53f3029e6831f2ec8
Nik Jakimovec <imkolias@gmail.com>
2023-06-17 19:32:30 +0300

    Sprint9 mongo fix
    Took 2 minutes

--- analytic-solution/api/v1/views.py
+++ analytic-solution/api/v1/views.py
@@ -9,7 +9,6 @@ from db.redis import get_redis
 router = APIRouter()
 
 BOOKMARK_MARK = 10
-PLUS_SIGN = '+'
 
 
 @router.get('/get_last_event', response_model=UserView)
@@ -37,8 +36,7 @@ async def get_views(
     user_id -- ID of user. Used as a part of key
     movie_id ID of movie. Used as a part of key
     """
-
-    key = f"{event_type}{PLUS_SIGN}{user_id}{PLUS_SIGN}{movie_id}"
+    key = f"{event_type}+{user_id}+{movie_id}"
     cache_data = await cache.get(key)
     film_frame = UserView(id=key, film_frame=0)
     if cache_data:
