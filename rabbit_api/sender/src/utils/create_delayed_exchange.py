ef38ea7bc9c2c3d7a7ce59b46d9d568bd7bf88cd
Nik Jakimovec <imkolias@gmail.com>
2023-06-23 19:48:25 +0300

    s10 Rabbit delayed_queue
    Took 51 minutes

--- rabbit_api/sender/src/utils/create_delayed_exchange.py
+++ rabbit_api/sender/src/utils/create_delayed_exchange.py
@@ -3,15 +3,27 @@ import os
 import pika
 from dotenv import load_dotenv
 
+print("CREATE DELAYED EXCHANGE")
+
+# RABBIT_USER="guest"
+# RABBIT_PASSWORD="guest"
+# RABBIT_HOST="localhost"
+# RABBIT_PORT=5672
+# RABBIT_EXCHANGE=""
+# RABBIT_QUEUE="sender"
+
+
 load_dotenv()
 rabbit_user = os.environ.get('RABBIT_USER')
 rabbit_psw = os.environ.get('RABBIT_PASSWORD')
 rabbit_host = os.environ.get('RABBIT_HOST')
 rabbit_port = os.environ.get('RABBIT_PORT')
 
+print(rabbit_host, rabbit_user, rabbit_psw, rabbit_port)
+
 # RabbitMQ setup
-credentials = pika.PlainCredentials(rabbit_user, rabbit_psw)
-connection_params = pika.ConnectionParameters(rabbit_host, rabbit_port, '/', credentials)
+credentials = pika.PlainCredentials(RABBIT_USER, RABBIT_PASSWORD)
+connection_params = pika.ConnectionParameters(RABBIT_HOST, RABBIT_PORT, '/', credentials)
 connection = pika.BlockingConnection(connection_params)
 channel = connection.channel()
 
