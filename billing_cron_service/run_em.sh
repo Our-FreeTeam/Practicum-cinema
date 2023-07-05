bff2e34a57961a5461e6b89fcfcceb1e01a63893
iana.volia <volyayana@gmail.com>
2023-07-05 20:02:14 +0300

    Structure for billing_cron_service

---
+++ billing_cron_service/run_em.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# TODO: изменить название скрипта
+python3 /opt/app/utils/email_to_queue.py
---
+++ billing_cron_service/utils/settings.py
@@ -0,0 +1,28 @@
+from pydantic import BaseSettings, Field
+
+
+class Settings(BaseSettings):
+    keycloak_url: str = Field(..., env='KEYCLOAK_URL')
+    client_id: str = Field(..., env='KEYCLOAK_CLIENT_ID')
+    realm_name: str = Field(..., env='KEYCLOAK_REALM_ID')
+    client_secret_key: str = Field(..., env='KEYCLOAK_SECRET_KEY')
+    service_account: str = Field(..., env='KEYCLOAK_SERVICE_ACCOUNT')
+
+    keycloak_admin_login: str = Field(..., env='KEYCLOAK_ADMIN')
+    keycloak_admin_password: str = Field(..., env='KEYCLOAK_ADMIN_PASSWORD')
+    keycloak_service_email: str = Field(..., env='KEYCLOAK_SERVICE_EMAIL')
+    log_format: str = Field(..., env='LOG_FORMAT')
+    log_level: int = Field(..., env='LOG_LEVEL')
+
+    keycloak_realm_su: str = Field(..., env='KEYCLOAK_CINEMAREALM_SU')
+    keycloak_realm_su_psw: str = Field(..., env='KEYCLOAK_CINEMAREALM_SU_PSW')
+
+    debug_mode: int = Field(0, env='BILLING_DEBUG')
+
+
+class Config:
+    env_file = '.env'
+    env_file_encoding = 'utf-8'
+
+
+settings = Settings()
