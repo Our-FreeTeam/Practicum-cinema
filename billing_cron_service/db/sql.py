b219b5c36e41f14d96f898ba5e8e3e316f383c45
Полина <PGVasilyeva@sberbank.ru>
2023-07-06 07:16:32 +0300

    fix

--- billing_cron_service/db/sql.py
+++ billing_cron_service/db/sql.py
@@ -1,5 +1,5 @@
 sql = """
-    SELECT sub.user_id, sub.subscription_type, sub_types.name, sub_types.amount
+    SELECT sub.person_id, sub.subscription_type, sub_types.name, sub_types.amount
       FROM content.subscriptions sub
       LEFT JOIN content.subscription_types sub_types on sub.id = sub_types.subscription_id
      WHERE DATEDIFF(day, datetime.datetime.now(), sub.end_date) < 4 AND sub.is_active = 1
