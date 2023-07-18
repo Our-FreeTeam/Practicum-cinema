09185117a093685419664e9252f342a62d9629d4
Sly_fox <tatsuchan@mail.ru>
2023-07-18 20:49:32 +0300

    поменяла переменную админского пароля из киклоака, которая удалилась в прошлых ПР
    сделала ручку для статистики с фильтрацией, query-параметрами и order by
    
    добавила декоратор проверки роли пользователя
    
    # Conflicts:
    #	billing_service/requirements.txt

--- tests_billing/functional/src/test_statistics.py
+++ tests_billing/functional/src/test_statistics.py
@@ -87,3 +87,79 @@ async def test_get_payment_statistic(
     response = requests.get(sub_url + url, json=body, headers=result.headers)
     assert response.status_code == answer_code
     assert len(response.json()["items"]) > 0
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    'answer_code, req_type, api_url, body',
+    [
+        (200, 'POST', 'v1/auth/login', {"user": "cinema_admin", "password": "password"}),
+        (200, 'POST', 'v1/admin/grant_role', {"user_name": "test_user", "role_name": "statistic_manager"}),
+    ],
+)
+@pytest.mark.asyncio
+async def test_fail_subscription_statistic(
+        answer_code,
+        req_type,
+        api_url,
+        body,
+        create_remove_test_user_and_role
+):
+    site_url = settings.auth_url
+    url_params = {
+        "url": site_url + api_url,
+        "json": body,
+        "headers": headers
+    }
+    result = requests.post(**url_params)
+    if result.headers.get("access_token") is not None and result.headers.get("refresh_token") is not None:
+        headers["access_token"] = result.headers.get("access_token")
+        headers["refresh_token"] = result.headers.get("refresh_token")
+    assert result.status_code == answer_code
+    body_get = {
+        "start_date": 3,
+        "is_active": "True",
+        "page": 1,
+        "size": 50
+    }
+    url = f"api/v1/statistics/subscriptions"
+    response = requests.get(sub_url + url, json=body_get, headers=result.headers)
+    assert response.status_code != HTTPStatus.OK
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    'answer_code, req_type, api_url, body',
+    [
+        (200, 'POST', 'v1/auth/login', {"user": "cinema_admin", "password": "password"}),
+        (200, 'POST', 'v1/admin/grant_role', {"user_name": "test_user", "role_name": "statistic_manager"}),
+    ],
+)
+@pytest.mark.asyncio
+async def test_fail_payment_statistic(
+        answer_code,
+        req_type,
+        api_url,
+        body,
+        create_remove_test_user_and_role
+):
+    site_url = settings.auth_url
+    url_params = {
+        "url": site_url + api_url,
+        "json": body,
+        "headers": headers
+    }
+    result = requests.post(**url_params)
+    if result.headers.get("access_token") is not None and result.headers.get("refresh_token") is not None:
+        headers["access_token"] = result.headers.get("access_token")
+        headers["refresh_token"] = result.headers.get("refresh_token")
+    assert result.status_code == answer_code
+    body_get = {
+        "subscription_id": 3,
+        "order_by": "payment_date",
+        "page": 1,
+        "size": 50
+    }
+    url = f"api/v1/statistics/payments"
+    response = requests.get(sub_url + url, json=body_get, headers=result.headers)
+    assert response.status_code != HTTPStatus.OK
